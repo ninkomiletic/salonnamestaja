2e1736ae4ea3b24dd4fcc8e1bf49f168ed039424
DESKTOP-B7RHLDO\Ninko <ninkomiletic@gmail.com>
2021-01-12 19:47:15 +0100

    Kreirani repozitorijumi u okviru Data Layera

---
+++ PresentationLayer/DataAccessLayer/Models/Korpa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    class Korpa
+    {
+        public int Id { get; set; }
+        public decimal racun { get; set; }
+    }
+}
---
+++ PresentationLayer/DataAccessLayer/NamestajRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class NamestajRepository
+    {
+        public object DBConnection { get; private set; }
+
+        public List<Namestaj> GetAllNamestaj()
+        {
+            List<Namestaj> results = new List<Namestaj>();
+
+            SqlDataReader sqlDataReader = DBconnection.GetData("SELECT * FROM Korisnik");
+
+            while (sqlDataReader.Read())
+            {
+                Namestaj n = new Namestaj();
+                n.Id = sqlDataReader.GetInt32(0);
+                n.Naziv = sqlDataReader.GetString(1);
+                n.Velicina = sqlDataReader.GetDecimal(2);
+                n.Cena = sqlDataReader.GetDecimal(3);
+                n.Stanje = sqlDataReader.GetInt32(4);
+                
+                results.Add(n);
+            }
+
+            DBconnection.CloseConnection();
+
+            return results;
+        }
+
+    }
+}
