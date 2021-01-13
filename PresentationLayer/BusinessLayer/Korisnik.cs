11f67762292fe9bff432462559aad389595eca18
sasamilinkovic72@gmail.com <sasamilinkovic72@gmail.com>
2021-01-13 21:48:57 +0100

    Buisness Layer Zavrsen

---
+++ PresentationLayer/BusinessLayer/Korisnik.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessLayer
+{
+    public class Korisnik
+    {
+
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string JMBG { get; set; }
+        public string Adresa { get; set; }
+        public string Telefon { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
